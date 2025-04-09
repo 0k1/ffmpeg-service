19e7922eb99bb4ce78e1aea3f914d0274c27ec0c
Arun Raj <arun.raj@sentinelprotocol.io>
2025-04-10 00:11:36 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,11 @@ module.exports = {
       {
         name: "ffmpeg-service",
         script: "./dist/server.js",
-        instances: "max",
+        instances: "1",
         exec_mode: "cluster",
         autorestart: true,
         watch: false,
-        max_memory_restart: "1G",
+        max_memory_restart: "4G",
         env: {
           NODE_ENV: "production",
         },
